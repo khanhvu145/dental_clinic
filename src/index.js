4b766662f42fcd2b3876e635b0cfc40819261b5e
Khanh Vũ <khanhvn14052000@gmail.com>
2021-08-22 11:05:20 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('hbs', handlebars({
+    //Đổi cấu trúc file
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
